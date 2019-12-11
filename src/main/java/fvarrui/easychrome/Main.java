61315504d6b5998968db3da7fb082c049108f00a
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2019-12-11 20:31:08 +0000

    U

--- src/main/java/fvarrui/easychrome/Main.java
+++ src/main/java/fvarrui/easychrome/Main.java
@@ -11,26 +11,25 @@ import su.litvak.chromecast.api.v2.Status;
 public class Main {
 
 	public static void main(String[] args) throws IOException, InterruptedException, GeneralSecurityException {
+
+		ChromeCast cc = new ChromeCast("Chromecast");
+
+		cc.connect();
+
+		// Get device status
+		Status status = cc.getStatus();
+		System.out.println(status);
+
+		if (cc.isAppAvailable("E8C28D3C") && !status.isAppRunning("E8C28D3C")) {
+			Application app = cc.launchApp("E8C28D3C");
+		}
+
+		cc.load("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+
+		cc.play();
 		
-		ChromeCast cc = new ChromeCast("Clase");
-		System.out.println(cc.getStatus());
-		
-//		ChromeCasts.startDiscovery();
-//		
-//		Thread.sleep(5000L);
-//		
-//		System.out.println(ChromeCasts.get());
-		
-//		// Connect (optional) 
-//		// Needed only when 'autoReconnect' is 'false'. 
-//		// Usually not needed and connection will be established automatically.
-//		 chromecast.connect();
-//		// Get device status
-//		Status status = chromecast.getStatus();
-//		// Run application if it's not already running
-//		if (chromecast.isAppAvailable("APP_ID") && !status.isAppRunning("APP_ID")) {
-//		  Application app = chromecast.launchApp("APP_ID");
-//		}
+		cc.disconnect();
+
 	}
 
 }
