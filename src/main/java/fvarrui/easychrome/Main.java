f9e9b3114a566cc01df434ec403913ebe8d6f22c
fvarrui <4354486+fvarrui@users.noreply.github.com>
2022-01-25 00:35:02 +0000

    U

--- src/main/java/fvarrui/easychrome/Main.java
+++ src/main/java/fvarrui/easychrome/Main.java
@@ -11,18 +11,30 @@ import su.litvak.chromecast.api.v2.Status;
 public class Main {
 
 	public static void main(String[] args) throws IOException, InterruptedException, GeneralSecurityException {
-
-		ChromeCast cc = new ChromeCast("Cocina");
+		
+//		ChromeCasts.startDiscovery();
+//
+//		Thread.sleep(30000L);
+//		
+//		System.out.println(ChromeCasts.get());
+//		
+//		ChromeCasts.stopDiscovery();
+		
+		ChromeCast cc = new ChromeCast("192.168.0.140");
 
 		cc.connect();
+		System.out.println(cc);
 
 		// Get device status
 		Status status = cc.getStatus();
 		System.out.println(status);
 
-//		if (cc.isAppAvailable("E8C28D3C") && !status.isAppRunning("E8C28D3C")) {
-//			Application app = cc.launchApp("E8C28D3C");
-//		}
+		if (cc.isAppAvailable("E8C28D3C") && !status.isAppRunning("E8C28D3C")) {
+			Application app = cc.launchApp("E8C28D3C");
+			System.out.println(app);
+		} else {
+			System.out.println("no app");
+		}
 
 		cc.load("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
 
